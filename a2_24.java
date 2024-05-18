bbb7cb16fbf5f03169cd76348ed6f0801274df96
Radhika Khattar <120930996+radhika143789@users.noreply.github.com>
2024-05-18 23:47:31 +0530

    Create a2_24.java
    Take integer inputs till the user enters 0 and print the sum of all numbers

---
+++ a2_24.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+class Scratch {
+    public static void main(String[] args) {
+        Scanner in=new Scanner(System.in);
+        System.out.println("Enter the number");
+        int n;
+        int sum =0;
+        while( (n= in.nextInt())!=0){
+            System.out.println("Enter the number");
+
+
+               sum += n;
+
+            }
+        System.out.println("The sum is");
+        System.out.println(sum);
+        }
+
+
+
+
+
+
+
+            }
+
+
+
+
